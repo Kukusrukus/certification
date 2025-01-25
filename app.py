47730b3ba82a5f2c4243fd09aef8772f5ed0b42f
Alexander Popov <95573726+Kukusrukus@users.noreply.github.com>
2025-01-26 00:03:33 +0300

    Update app.py
    render PORT - string 111

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def index():
 
     return render_template("index.html", error_message=error_message)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
